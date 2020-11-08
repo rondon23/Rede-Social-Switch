853b8139e3efe1e998f2b37182f531fd54c99efd
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-08 12:39:06 -0400

    Adicionando a entidade de status relacionamento e aplicando a migtation.

---
+++ Switch.Infra.Data/Migrations/20201108161826_AdicionadaStatusRelacionamento.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AdicionadaStatusRelacionamento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StatusRelacionamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusRelacionamentos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StatusRelacionamentos");
+        }
+    }
+}
---
+++ Switch.Infra.Data/Migrations/20201108163516_AdicionadaStatusRelacionamentoDescricao.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AdicionadaStatusRelacionamentoDescricao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StatusRelacionamentos",
+                table: "StatusRelacionamentos");
+
+            migrationBuilder.RenameTable(
+                name: "StatusRelacionamentos",
+                newName: "StatusRelacionamento");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "StatusRelacionamento",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StatusRelacionamento",
+                table: "StatusRelacionamento",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_StatusRelacionamento",
+                table: "StatusRelacionamento");
+
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "StatusRelacionamento");
+
+            migrationBuilder.RenameTable(
+                name: "StatusRelacionamento",
+                newName: "StatusRelacionamentos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_StatusRelacionamentos",
+                table: "StatusRelacionamentos",
+                column: "Id");
+        }
+    }
+}
