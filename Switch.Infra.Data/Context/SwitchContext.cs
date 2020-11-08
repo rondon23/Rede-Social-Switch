ddf8a9e8193a005f050859d3bdc7f27b3998253d
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-08 07:06:42 -0400

    adicionando o switch context.

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchContext : DbContext
+    {
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        public SwitchContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
