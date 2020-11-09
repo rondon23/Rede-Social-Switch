cebe487652a215a762f411be385a0d96e73057e5
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-08 21:52:12 -0400

    Relacionamento um para muitos

--- Switch.Infra.Data/Config/UsuarioConfiguration.cs
+++ Switch.Infra.Data/Config/UsuarioConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
 using Switch.Domain.Entities;
 
 namespace Switch.Infra.Data.Config
@@ -9,33 +10,45 @@ namespace Switch.Infra.Data.Config
         public void Configure(EntityTypeBuilder<Usuario> builder)
         {
 
-            builder.ToTable("usuarios");
+            //builder.ToTable("usuarios");
 
-            builder.Property(e => e.Id).HasColumnType("int(11)");
+            //builder.Property(e => e.Id).HasColumnType("int(11)");
 
-            builder.Property(e => e.Email)
-                .IsRequired()
-                .HasColumnType("varchar(400)");
+            //builder.Property(e => e.Email)
+            //    .IsRequired()
+            //    .HasColumnType("varchar(400)");
 
-            builder.Property(e => e.Nome)
-                .IsRequired()
-                .HasColumnType("varchar(400)");
+            //builder.Property(e => e.Nome)
+            //    .IsRequired()
+            //    .HasColumnType("varchar(400)");
 
-            builder.Property(e => e.Senha)
-                .IsRequired()
-                .HasColumnType("varchar(400)");
+            //builder.Property(e => e.Senha)
+            //    .IsRequired()
+            //    .HasColumnType("varchar(400)");
 
-            builder.Property(e => e.SobreNome)
-                .IsRequired()
-                .HasColumnType("varchar(400)");
+            //builder.Property(e => e.SobreNome)
+            //    .IsRequired()
+            //    .HasColumnType("varchar(400)");
 
-            builder.Property(e => e.UrlFoto)
-                .IsRequired()
-                .HasColumnType("varchar(400)");
+            //builder.Property(e => e.UrlFoto)
+            //    .IsRequired()
+            //    .HasColumnType("varchar(400)");
+
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Nome).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.SobreNome).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Email).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Senha).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.Sexo).IsRequired();
+            builder.Property(u => u.UrlFoto).HasMaxLength(400).IsRequired();
+            builder.Property(u => u.DataNascimento).IsRequired();
 
             builder.HasOne(u => u.Identificacao)
                 .WithOne(i => i.Usuario)
                 .HasForeignKey<Identificacao>(i => i.UsuarioId);
+
+            builder.HasMany(u => u.Postagens).WithOne(p => p.Usuario);
+
         }
     }
 }
---
+++ Switch.Infra.Data/Migrations/20201109014942_AddRelacionamentoPostagem2.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Switch.Infra.Data.Migrations
+{
+    public partial class AddRelacionamentoPostagem2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Identificacao_usuarios_UsuarioId",
+                table: "Identificacao");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_postagens_usuarios_UsuarioId",
+                table: "postagens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_usuarios",
+                table: "usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "usuarios",
+                newName: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int(11)")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "postagens",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int(11)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Identificacao",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int(11)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Identificacao_Usuarios_UsuarioId",
+                table: "Identificacao",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_postagens_Usuarios_UsuarioId",
+                table: "postagens",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Identificacao_Usuarios_UsuarioId",
+                table: "Identificacao");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_postagens_Usuarios_UsuarioId",
+                table: "postagens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UrlFoto",
+                table: "usuarios",
+                type: "varchar(400)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SobreNome",
+                table: "usuarios",
+                type: "varchar(400)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "usuarios",
+                type: "varchar(400)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "usuarios",
+                type: "varchar(400)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "usuarios",
+                type: "varchar(400)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 400);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "usuarios",
+                type: "int(11)",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "postagens",
+                type: "int(11)",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Identificacao",
+                type: "int(11)",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_usuarios",
+                table: "usuarios",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Identificacao_usuarios_UsuarioId",
+                table: "Identificacao",
+                column: "UsuarioId",
+                principalTable: "usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_postagens_usuarios_UsuarioId",
+                table: "postagens",
+                column: "UsuarioId",
+                principalTable: "usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
