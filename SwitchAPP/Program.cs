4baa362944ef96b8230aa27dfbb76c339865c495
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2020-11-14 20:21:32 -0400

    Inserindo um objeto.

--- SwitchAPP/Program.cs
+++ SwitchAPP/Program.cs
@@ -9,15 +9,37 @@ namespace SwitchAPP
     {
         static void Main(string[] args)
         {
-            var usuario = new Usuario() { Nome = "Usuario1", SobreNome = "SobreUsuario" };
+            var usuario = new Usuario() 
+            { 
+                Nome = "Usuario1", 
+                SobreNome = "SobreUsuario",
+                Senha = "abc123",
+                Email = "usuario@teste.com,", 
+                DataNascimento = DateTime.Now,
+                Sexo = Switch.Domain.Enums.SexoEnum.Masculino,
+                UrlFoto = @"c:\temp"};
+
             var optionsBuilder = new DbContextOptionsBuilder<SwitchContext>();
             optionsBuilder.UseLazyLoadingProxies();
             optionsBuilder.UseMySql("Server=localhost;userid=developer;password=1234567;database=SwitchDB", m => m.MigrationsAssembly("Switch.Infra.Data"));
-            
-            using (var dbcontext = new SwitchContext(optionsBuilder.Options))
+
+            try
             {
-                dbcontext.Usuarios.Add(usuario);
+                using (var dbcontext = new SwitchContext(optionsBuilder.Options))
+                {
+                    dbcontext.Usuarios.Add(usuario);
+                    dbcontext.SaveChanges();
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Ok!");
+            Console.ReadKey();
+
         }
     }
 }
